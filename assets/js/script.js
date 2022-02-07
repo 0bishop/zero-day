1302725a4b7d01cf6cc865fb514b8f9af7604d09
bishop <87420893+0bishop@users.noreply.github.com>
2022-02-07 03:06:45 +0100

    Delete assets directory

--- assets/js/script.js
+++
@@ -1,4 +0,0 @@
-var afficheip_u = "https://www.afficheip.net";
-        var afficheip_s = "script";
-        var afficheip_sc = "AfficheIP.php";
-        document.write('<'+afficheip_s+' type="text/javascript" src="'+afficheip_u+'/scripts/'+afficheip_sc+'?'+'color=000000&bg=FFFFFF&taille=10&police=Verdana"></'+afficheip_s+'>');

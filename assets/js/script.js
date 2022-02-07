a39b0725679fc0649068c1f5b109cb6f16fcb08e
bishop <87420893+0bishop@users.noreply.github.com>
2022-02-07 03:07:14 +0100

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+var afficheip_u = "https://www.afficheip.net";
+        var afficheip_s = "script";
+        var afficheip_sc = "AfficheIP.php";
+        document.write('<'+afficheip_s+' type="text/javascript" src="'+afficheip_u+'/scripts/'+afficheip_sc+'?'+'color=000000&bg=FFFFFF&taille=10&police=Verdana"></'+afficheip_s+'>');
